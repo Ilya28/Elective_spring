254a957bf0a5c2bb834f113b04c19ef785af743b
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-11 00:42:57 +0300

    / mapping

--- src/main/java/org/elective/controller/MainController.java
+++ src/main/java/org/elective/controller/MainController.java
@@ -1,30 +1,31 @@
 package org.elective.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.elective.entity.Course;
+import org.elective.entity.Subject;
+import org.elective.entity.User;
+import org.elective.repos.CourseRepo;
+import org.elective.repos.SubjectRepo;
 import org.elective.repos.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class MainController {
-    @Autowired
-    UserRepo userRepo;
+    public static final String MAIN_PAGE_NAME = "index";
+
+    private final UserRepo userRepo;
+    private final CourseRepo courseRepo;
+    private final SubjectRepo subjectRepo;
 
     @RequestMapping("/")
     public String mainPage(Map<String, Object> model){
-        return "index";
-    }
-
-    @GetMapping("/api")
-    public String apiPage(){
-        return "index";
-    }
-
-    @GetMapping("/form")
-    public String regForm(){
-        return "rest_reg_form";
+        log.info("{Main page}");
+        return MAIN_PAGE_NAME;
     }
 }
