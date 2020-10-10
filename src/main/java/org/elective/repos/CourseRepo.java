19448068f6eb300b5f3a51681a2d52f6a13bf33c
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-11 00:37:21 +0300

    added some "findBy..." methods

--- src/main/java/org/elective/repos/CourseRepo.java
+++ src/main/java/org/elective/repos/CourseRepo.java
@@ -1,8 +1,15 @@
 package org.elective.repos;
 
-import org.elective.domain.dto.Course;
+import org.elective.entity.Course;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CourseRepo  extends CrudRepository<Course, Long> {
-
+    public List<Course> findCoursesBySubjectId(Integer id);
+    public List<Course> findCoursesBySubjectNameEN(String name);
+    Optional<Course> findCourseBySubjectNameEN(String name);
+    Optional<Course> findCourseByNameEN(String name);
 }
