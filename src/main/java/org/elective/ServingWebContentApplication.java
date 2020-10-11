47b8596a33670eed37d23acb1ae16e426526ab32
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-11 12:13:57 +0300

    refactor

--- src/main/java/org/elective/ServingWebContentApplication.java
+++ src/main/java/org/elective/ServingWebContentApplication.java
@@ -2,20 +2,10 @@ package org.elective;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
-@SpringBootApplication(
-        //exclude = {DataSourceAutoConfiguration.class},
-        scanBasePackages={
-                "org.elective.repos",
-                "org.elective.domain.dto"
-        }
-)
-@PropertySources({
-        @PropertySource(value = "classpath:application.properties")
-})
+@SpringBootApplication(scanBasePackages={"org.elective"})
+@PropertySource(value = "classpath:application.properties")
 public class ServingWebContentApplication {
 
     public static void main(String[] args) {
