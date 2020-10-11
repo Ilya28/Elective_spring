9dc4b983102a7f4ffbc78eb341e045e8d613f9d4
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-11 18:32:03 +0300

    disabled

--- src/main/java/org/elective/config/WebSecurityConfig.java
+++ src/main/java/org/elective/config/WebSecurityConfig.java
@@ -1,31 +1,5 @@
 package org.elective.config;
-n;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                    .antMatchers("/", "/home").permitAll()
-                    .anyRequest().authenticated()
-                .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                .and()
-                    .logout()
-                    .permitAll();
-    }
+public class WebSecurityConfig {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //auth
-    }
 }
