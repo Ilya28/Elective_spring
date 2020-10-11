4361e8c892a67d60dcafc656fca4c0197925d030
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-11 18:28:31 +0300

    another impl

--- src/main/resources/static/js/registration_action.js
+++ src/main/resources/static/js/registration_action.js
@@ -2,40 +2,19 @@ angular.module("login_form",[])
     .controller("LogCtrl", function ($scope, $http) {
         $scope.auth = {};
         $scope.sendForm = function(auth){
-            $http({
-                method: "POST",
-                url: "/login",
-                data: $.param(auth),
-                headers: { "Content-Type" : "application/x-www-form-urlencoded" }
-            }).then(
-                function(data) {
-                    window.alert("Доступ разрешен");
-                },
-                function(error) {
-                    window.alert("Доступ запрещен");
-                }
-            );
+            const parameter = JSON.stringify({name: auth.name, email: auth.email, password: auth.password});
+            $http.post("/en/login", parameter);
         }
     })
     .controller("RegCtrl", function ($scope, $http) {
         $scope.auth = {};
         $scope.sendForm = function(auth){
-            //if (!$scope.auth.password !== document.getElementsByName("confirm-password")[0].value) {
-            //    $scope.emailRequired = 'Email confirmation error';
-            //} else {
-                $http({
-                    method: "POST",
-                    url: "/meow",
-                    data: $.param(auth),
-                    headers: {"Content-Type": "application/x-www-form-urlencoded"}
-                }).then(
-                    function (data) {
-                        window.alert("Доступ разрешен");
-                    },
-                    function (error) {
-                        window.alert("Доступ запрещен");
-                    }
-                );
-            //}
+            if ($scope.auth.password !== document.getElementsByName("confirm-password")[0].value) {
+                $scope.emailRequired = 'Email confirmation error';
+                window.alert("Email confirmation error");
+            } else {
+                const parameter = JSON.stringify({name: auth.name, email: auth.email, password: auth.password});
+                $http.post("/en/register", parameter);
+            }
         }
     });
