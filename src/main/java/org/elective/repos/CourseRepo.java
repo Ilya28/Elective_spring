ccd4f21199be7f89742975ae8b6fd569f194426f
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-11 12:16:42 +0300

    findAll method

--- src/main/java/org/elective/repos/CourseRepo.java
+++ src/main/java/org/elective/repos/CourseRepo.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CourseRepo  extends CrudRepository<Course, Long> {
     public List<Course> findCoursesBySubjectId(Integer id);
     public List<Course> findCoursesBySubjectNameEN(String name);
+    public List<Course> findAll();
     Optional<Course> findCourseBySubjectNameEN(String name);
     Optional<Course> findCourseByNameEN(String name);
 }
