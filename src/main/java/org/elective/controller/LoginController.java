0bb7864b66c8d2c4ca22347071f43b98cb6b20da
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-11 18:30:20 +0300

    login and register mapping with prototypes of handlers

--- src/main/java/org/elective/controller/LoginController.java
+++ src/main/java/org/elective/controller/LoginController.java
@@ -3,23 +3,35 @@ package org.elective.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.elective.dto.UserDTO;
-import org.elective.dto.UsersDTO;
+import org.elective.service.LoginService;
 import org.elective.service.UserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.elective.entity.User;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/{locale:\\ben|ua\\b}")
 public class LoginController {
-    private final UserService userService; // Autowired
+    private final UserService userService;
+    private final LoginService loginService;
 
     @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/register")
+    public ResponseEntity<String> registerFormController(@RequestBody UserDTO auth){
+        log.info("{new user}");
+        boolean result = loginService.saveUser(auth);
+        log.info(result ? "{new user added}": "{user with this email already exist}");
+        log.info("{}", auth);
+        return new ResponseEntity<>("Success", HttpStatus.OK);
+    }
+
     @PostMapping(value = "/login")
-    public void loginFormController(UserDTO user){
-        log.info("{}",userService.findByUserLogin(user));
-        log.info("{}", user);
+    public ResponseEntity<String> loginFormController(@RequestBody UserDTO auth){
+
+        log.info("{}",userService.findByUserLogin(auth));
+        log.info("{}", auth);
+        return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 }
