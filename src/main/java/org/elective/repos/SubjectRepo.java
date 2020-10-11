55c10ef825f16d2f9c0a88dab9b0f7aa01bd4b1c
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-11 12:13:37 +0300

    findAll method

--- src/main/java/org/elective/repos/SubjectRepo.java
+++ src/main/java/org/elective/repos/SubjectRepo.java
@@ -2,9 +2,13 @@ package org.elective.repos;
 
 import org.elective.entity.Subject;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface SubjectRepo  extends CrudRepository<Subject, Long> {
     public Optional<Subject> findSubjectByNameEN(String name);
+    List<Subject> findAll();
 }
