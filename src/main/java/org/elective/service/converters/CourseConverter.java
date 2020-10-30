e70554da934caef954ccc6aaaaa6c79f1b9b8e77
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-30 02:29:29 +0200

    coursesToCoursesDTO and courseDTOToCourse methods, id and subjectMapping in courseToCourseDTO method

--- src/main/java/org/elective/service/converters/CourseConverter.java
+++ src/main/java/org/elective/service/converters/CourseConverter.java
@@ -4,9 +4,13 @@ import org.elective.dto.CourseDTO;
 import org.elective.entity.Course;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CourseConverter {
-    public CourseDTO CourseToCourseDTO(Course course, String locale) {
+    public CourseDTO courseToCourseDTO(Course course, String locale) {
         String i18nDescription = "ua".equalsIgnoreCase(locale)?
                 course.getDescriptionUA(): course.getDescriptionEN();
         String i18nName = "ua".equalsIgnoreCase(locale)?
@@ -14,15 +18,36 @@ public class CourseConverter {
         String i18nSubject = "ua".equalsIgnoreCase(locale)?
                 course.getSubject().getNameUA(): course.getSubject().getNameEN();
         return CourseDTO.builder()
+                .id(course.getId())
                 .backgroundFile(course.getBackgroundFile())
                 .dateStart(course.getDateStart().toString())
                 .dateEnd(course.getDateEnd().toString())
-                .mapping(course.getMapping())
                 .description(i18nDescription)
                 .name(i18nName)
                 .seats(course.getSeats())
                 .signedUp(course.getSignedUp())
                 .subject(i18nSubject)
+                .subjectMapping(course.getSubject().getMapping())
+                .build();
+    }
+
+    public List<CourseDTO> coursesToCoursesDTO(List<Course> courses, String locale) {
+        return courses.stream()
+                .map(c -> courseToCourseDTO(c, locale))
+                .collect(Collectors.toList());
+    }
+
+    public Course courseDTOToCourse(CourseDTO courseDTO) {
+        return Course.builder()
+                .nameEN(courseDTO.getNameEN())
+                .nameUA(courseDTO.getNameUA())
+                .backgroundFile(courseDTO.getBackgroundFile())
+                .dateStart(LocalDate.parse(courseDTO.getDateStart()))
+                .dateEnd(LocalDate.parse(courseDTO.getDateEnd()))
+                .seats(courseDTO.getSeats())
+                .descriptionEN(courseDTO.getDescriptionEN())
+                .descriptionUA(courseDTO.getDescriptionUA())
+                .signedUp(courseDTO.getSignedUp())
                 .build();
     }
 }
