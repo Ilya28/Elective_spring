fdcebf8332100636b30e6d8bf44e8a89d72d12b7
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-30 02:43:57 +0200

    login json sender removed

--- src/main/resources/static/js/registration_action.js
+++ src/main/resources/static/js/registration_action.js
@@ -1,17 +1,17 @@
 angular.module("login_form",[])
-    .controller("LogCtrl", function ($scope, $http) {
+    /*.controller("LogCtrl", function ($scope, $http) {
         $scope.auth = {};
         $scope.sendForm = function(auth){
             const parameter = JSON.stringify({name: auth.name, email: auth.email, password: auth.password});
             $http.post("/en/login", parameter);
         }
-    })
+    })*/
     .controller("RegCtrl", function ($scope, $http) {
         $scope.auth = {};
         $scope.sendForm = function(auth){
             if ($scope.auth.password !== document.getElementsByName("confirm-password")[0].value) {
-                $scope.emailRequired = 'Email confirmation error';
-                window.alert("Email confirmation error");
+                //$scope.emailRequired = 'Password confirmation error';
+                window.alert("Password confirmation error");
             } else {
                 const parameter = JSON.stringify({name: auth.name, email: auth.email, password: auth.password});
                 $http.post("/en/register", parameter);
