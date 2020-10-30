c7beb3f86af4c3283ae3d29816419cdd4614ad78
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-30 02:49:44 +0200

    impl added

--- src/main/java/org/elective/config/WebSecurityConfig.java
+++ src/main/java/org/elective/config/WebSecurityConfig.java
@@ -1,5 +1,71 @@
 package org.elective.config;
 
-public class WebSecurityConfig {
+import lombok.extern.slf4j.Slf4j;
+import org.elective.service.secure.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        prePostEnabled = true
+)
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers("/css/**", "/js/**");
+    }
+
+    @Autowired
+    public void registerGlobalAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/", "/en/register").permitAll()
+                    .antMatchers("/users", "/users/**").hasRole("ADMIN")
+                    .anyRequest().authenticated()
+                .and()
+                    .httpBasic()
+                .and()
+                    .formLogin()
+                    .loginPage("/en/login")
+                    .loginProcessingUrl("/en/login")
+                    .defaultSuccessUrl("/en/home")
+                    .failureUrl("/en/login?login_error")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .permitAll()
+                .and()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .permitAll();
+        http.csrf().disable();
+    }
 }
