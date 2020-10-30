759f1d3d55ca96d05db7a9f64e4572599c6853b8
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-30 02:49:30 +0200

    findAll method added

--- src/main/java/org/elective/repos/UserRepo.java
+++ src/main/java/org/elective/repos/UserRepo.java
@@ -1,12 +1,16 @@
 package org.elective.repos;
 
 import org.elective.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepo extends CrudRepository<User, Long> {
-    public Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email);
+    Page<User> findAll(Pageable pageable);
 }
