f6b3c705bb5014783b9e583a185221603bbbe92c
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-30 02:50:08 +0200

    unused removed

--- src/main/java/org/elective/service/UserService.java
+++ src/main/java/org/elective/service/UserService.java
@@ -1,51 +1,49 @@
 package org.elective.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.elective.dto.UserDTO;
-import org.elective.dto.UsersDTO;
+import org.elective.entity.Registration;
 import org.elective.entity.User;
+import org.elective.repos.RegistrationRepo;
 import org.elective.repos.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.elective.service.converters.UserConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    public static final int USERS_PER_PAGE = 16;
+    private final UserRepo userRepo;
+    private final UserConverter userConverter;
+    private final RegistrationRepo registrationRepo;
 
-    private final UserRepo userRepository;
-
-    @Autowired
-    public UserService(UserRepo userRepository) {
-        this.userRepository = userRepository;
+    public Optional<User> findByEmail(String email){
+        return userRepo.findByEmail(email);
     }
 
-    public UsersDTO getAllUsers() {
-        //TODO checking for an empty user list
-        return new UsersDTO(userRepository.findAll());
+    public Optional<UserDTO> getUserById(Long id) {
+        Optional<User> user = userRepo.findById(id);
+        return user.map(userConverter::userToUserDTO);
     }
 
-    public Optional<User> findByUserLogin (UserDTO userDTO){
-        //TODO check for user availability. password check
-        return userRepository.findByEmail(userDTO.getEmail());
+    /**
+     * The method is used to save changes to user parameters.
+     * First you need to make sure that an object with such an ID exists.
+     * Further, the DTO object is transformed into an entity.
+     * After that, the object (entity) can be saved to the repository.
+     * @param userDTO - This is a DTO object whose data should be used to update a record in a table.
+     */
+    @Transactional
+    public void modifyUser(UserDTO userDTO) {
+        Optional<User> user = userRepo.findById(userDTO.getId());
+        if (!user.isPresent())
+            return;
+        User modifiedUser = userConverter.UserDTOToUser(userDTO);
+        modifiedUser.setPassword(user.get().getPassword());
+        userRepo.save(modifiedUser);
     }
-
-    public void saveNewUser (User user){
-        //TODO inform the user about the replay email
-        // TODO exception to endpoint
-        try {
-            userRepository.save(user);
-        } catch (Exception ex){
-            log.info("{Почтовый адрес уже существует}");
-        }
-
-    }
-
-    public int getCalculatePageCount(int usersCount) {
-        return usersCount % USERS_PER_PAGE == 0? usersCount / USERS_PER_PAGE: usersCount / USERS_PER_PAGE + 1;
-    }
-
-
 }
