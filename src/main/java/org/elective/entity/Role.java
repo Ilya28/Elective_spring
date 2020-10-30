9fa447de39adedf06fcb5c51db2dc13d313c9124
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-30 02:44:14 +0200

    implements GrantedAuthority

--- src/main/java/org/elective/entity/Role.java
+++ src/main/java/org/elective/entity/Role.java
@@ -1,5 +1,12 @@
 package org.elective.entity;
 
-public enum Role {
-    USER, TEACHER, ADMIN;
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role  implements GrantedAuthority {
+    ROLE_USER, ROLE_TEACHER, ROLE_ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
