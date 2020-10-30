1bcbce9ed3f6a986e51c3f1fde53f86c78127589
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-30 02:46:43 +0200

    localized fields and validation added

--- src/main/java/org/elective/dto/SubjectDTO.java
+++ src/main/java/org/elective/dto/SubjectDTO.java
@@ -2,13 +2,21 @@ package org.elective.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class SubjectDTO {
-    private String name;
+    private String name; // localized
+    @NotBlank
+    private String nameEN;
+    @NotBlank
+    private String nameUA;
+    @Pattern(regexp = "[a-z_]")
     private String mapping;
     private String backgroundFile;
 }
