945b6c0c05164d253ce17059680b16be998cb099
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-30 02:31:09 +0200

    removed unused  methods

--- src/main/java/org/elective/repos/CourseRepo.java
+++ src/main/java/org/elective/repos/CourseRepo.java
@@ -1,17 +1,16 @@
 package org.elective.repos;
 
 import org.elective.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CourseRepo  extends CrudRepository<Course, Long> {
-    public List<Course> findCoursesBySubjectId(Integer id);
-    public List<Course> findCoursesBySubjectNameEN(String name);
-    public List<Course> findAll();
-    Optional<Course> findCourseBySubjectNameEN(String name);
+    Page<Course> findCoursesBySubjectMapping(String mapping, Pageable pageable);
+    public Page<Course> findAll(Pageable pageable);
     Optional<Course> findCourseByNameEN(String name);
 }
