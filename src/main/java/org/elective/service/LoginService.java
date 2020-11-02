98742e6c5b86addf87c87c4dbd9ecf147daedebe
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-02 22:51:38 +0200

    refactor

--- src/main/java/org/elective/service/LoginService.java
+++ src/main/java/org/elective/service/LoginService.java
@@ -1,25 +1,34 @@
 package org.elective.service;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.elective.dto.UserDTO;
 import org.elective.entity.User;
 import org.elective.repos.UserRepo;
 import org.elective.service.converters.UserConverter;
+import org.elective.service.preparing.LocalizationPreparingService;
+import org.elective.service.preparing.NavbarOnPageService;
+import org.elective.service.preparing.RoleOnPageService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Slf4j
-@RequiredArgsConstructor
 @Service
-public class LoginService {
+public class LoginService  extends AbstractService {
     private final UserRepo userRepo;
     private final UserConverter userConverter;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public LoginService(LocalizationPreparingService localeService, NavbarOnPageService navbarService,
+                        RoleOnPageService roleService, UserRepo userRepo, UserConverter userConverter,
+                        BCryptPasswordEncoder bCryptPasswordEncoder) {
+        super(localeService, navbarService, roleService);
+        this.userRepo = userRepo;
+        this.userConverter = userConverter;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
     @Transactional
     public boolean saveNewUser(UserDTO userDTO, String locale) {
