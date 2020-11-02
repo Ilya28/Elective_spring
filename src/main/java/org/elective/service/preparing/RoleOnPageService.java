3c3867496cde1c3461a4612d7bfc66e527ed4f36
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-02 22:54:39 +0200

    refactor

--- src/main/java/org/elective/service/preparing/RoleOnPageService.java
+++ src/main/java/org/elective/service/preparing/RoleOnPageService.java
@@ -2,10 +2,7 @@ package org.elective.service.preparing;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.elective.entity.Role;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -18,11 +15,6 @@ public class RoleOnPageService {
     private static final String DEFAULT_ROLE = "ROLE_ANONYMOUS";
 
     public void putRole(Map<String, Object> model) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        val authorities = auth.getAuthorities();
-        model.put(ROLE_HOLDER, authorities.stream()
-                .findFirst()
-                .map(GrantedAuthority::toString)
-                .orElse(DEFAULT_ROLE));
+        model.put(ROLE_HOLDER, Role.getCurrentUserRole());
     }
 }
